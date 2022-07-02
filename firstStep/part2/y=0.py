55978486fd234a7f348268d7015f7d85ba9e5723
aTaskaev <30393521+cartoonsub@users.noreply.github.com>
2022-07-02 19:39:23 +0300

    docAndPep

---
+++ firstStep/part2/y=0.py
@@ -0,0 +1,13 @@
+
+
+class Intetesting_function:
+    """
+    intetesting_function.py
+    """
+    for i in range(1, 11):
+        print(i)
+
+print(Intetesting_function.__doc__)
+
+import os
+print(os.getcwd.__doc__)

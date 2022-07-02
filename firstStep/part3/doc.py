6a30c332cb9db69beb1013147eeb05a2013302dc
aTaskaev <30393521+cartoonsub@users.noreply.github.com>
2022-07-02 20:33:56 +0300

    Create doc.py

---
+++ firstStep/part3/doc.py
@@ -0,0 +1,58 @@
+string = 'abcdefgc'
+
+print(string.find('c'))
+
+print(string.index('c'))
+
+print(string.startswith('a'))
+print(string.startswith(('ab', 'cd')))
+
+print(string.endswith('g'))
+
+print()
+
+print(string.count('c'))
+
+print(string.find('c'))
+print(string.rfind('c'))
+
+print('\n')
+
+string = 'The man in black fled across the desert, and th,e gunslinge,r followed'
+
+print(string.lower())
+print(string.upper())
+print(string.count('the'))
+print(string.lower().count('the'))
+
+print('\n')
+
+print(string.replace(',', ', ', 2))
+
+print('\n')
+
+print(string.split(' ', 5))
+
+print('\n')
+
+string = '       The man in black fled across the desert, and the gunslinger followed                    '
+
+print(string.strip())
+print(string.strip('The '))
+print(string.lstrip())
+print(string.rstrip())
+
+print('\n')
+
+print('=>'.join(['1', '2']))
+print(repr('=>'.join(['1', '2'])))
+
+number = map(str, range(10))
+print('=>'.join(number))
+
+print('\n')
+
+capital = 'London is the capital of Great Britain'
+template = '{} is the capital of {}'
+print(template.format('London', 'great britain'))
+print(template.format('vaduz', 'switzerland'))

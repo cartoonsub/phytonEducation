708a459f620cc864c2e681e347c082db588c940a
Any Task <t.series@yandex.ru>
2024-05-22 20:28:26 +0300

    sprint 2

---
+++ yandex/sprint_2/unlovedCase.py
@@ -0,0 +1,47 @@
+import os
+
+LOCAL = os.environ.get('REMOTE_JUDGE', 'false') != 'true'
+
+if LOCAL:
+    class Node:
+        def __init__(self, value, next_item=None):
+            self.value = value
+            self.next_item = next_item
+
+
+def solution(node, idx):
+    if idx == 0:
+        value = node.next_item.value
+        next_item = node.next_item.next_item
+        node.next_item = next_item
+        node.value = value
+        return node
+
+    node_number = 0
+    current_node = node
+    while current_node is not None:
+        if node_number + 1 != idx:
+            continue
+
+        next_item = current_node.next_item.next_item
+        current_node.next_item = next_item
+        break
+    
+    return node
+
+
+def test():
+    node3 = Node("node3", None)
+    node2 = Node("node2", node3)
+    node1 = Node("node1", node2)
+    node0 = Node("node0", node1)
+    new_head = solution(node0, 0)
+    assert new_head is node0
+    assert new_head.next_item is node2
+    assert new_head.next_item.next_item is node3
+    assert new_head.next_item.next_item.next_item is None
+    # result is node0 -> node2 -> node3
+
+
+if __name__ == '__main__':
+    test()

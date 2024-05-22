b485af3dc25ee6e3b42ba63d386b21696f45c02e
Any Task <t.series@yandex.ru>
2024-05-22 18:53:10 +0300

    sprint 2

---
+++ yandex/sprint_2/monitor.py
@@ -0,0 +1,12 @@
+
+
+rows = int(input())
+columns = str(input()).split(' ')
+
+matrix = {}
+for i in range(rows):
+    matrix[i] = input().split(' ')
+
+print(matrix)
+result = []
+print(' '.join(map(str, result)))

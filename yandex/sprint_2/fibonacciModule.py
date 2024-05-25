ac4c0cd2c2a3ab7148bb2a169eafab63015ad50b
Any Task <t.series@yandex.ru>
2024-05-26 00:50:12 +0300

    sprint 2

---
+++ yandex/sprint_2/fibonacciModule.py
@@ -0,0 +1,25 @@
+import time
+
+def fibonacci(n):
+    numbers = [1, 1]
+    if n < 2:
+        return 1
+    else:
+        counter = 0
+        while counter < n - 1:
+            summ = (numbers[0] + numbers[1])
+            numbers[0] = numbers[1]
+            numbers[1] = summ
+            fib = summ
+            counter += 1
+        return fib
+
+# number, commits = map(int, input().split())
+number, commits = map(int, '506277 6'.split())
+
+timeStart = time.time()
+result = fibonacci(number) % (10 ** commits)
+end_time = time.time()
+print(f"Time: {end_time - timeStart}")
+print(result)
+# Fn = Fn-1 + Fn-2 

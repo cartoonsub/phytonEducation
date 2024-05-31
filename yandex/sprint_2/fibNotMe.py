61cc931d7e884accdc19b49169fcea49c32fe91b
Any Task <t.series@yandex.ru>
2024-05-31 22:12:08 +0300

    Refactor Fibonacci calculation using matrix multiplication

--- yandex/sprint_2/fibNotMe.py
+++ yandex/sprint_2/fibNotMe.py
@@ -1,27 +1,63 @@
-def find_pisano_period(m):
-    previous, current = 0, 1
-    for i in range(0, m * m):
-        previous, current = current, (previous + current) % m
-        if (previous == 0 and current == 1):
-            return i + 1
+class MatrixFibonacci:
+    Q = [[1, 1],
+         [1, 0]]
 
-def fibonacci_modulo(n, m):
-    pisano_period = find_pisano_period(m)
-    n = n % pisano_period
+    def __init__(self):
+        self.__memo = {}
 
-    previous, current = 0, 1
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    def __multiply_matrices(self, M1, M2):
+        """Умножение матриц
+        (ожидаются матрицы в виде списка список размером 2x2)."""
 
-    for i in range(n - 1):
-        previous, current = current, (previous + current) % m
+        a11 = M1[0][0]*M2[0][0] + M1[0][1]*M2[1][0]
+        a12 = M1[0][0]*M2[0][1] + M1[0][1]*M2[1][1]
+        a21 = M1[1][0]*M2[0][0] + M1[1][1]*M2[1][0]
+        a22 = M1[1][0]*M2[0][1] + M1[1][1]*M2[1][1]
+        r = [[a11, a12], [a21, a22]]
+        return r
 
-    return current
+    def __get_matrix_power(self, M, p):
+        """Возведение матрицы в степень (ожидается p равная степени двойки)."""
 
-# Пример использования:
-n = 1000
-m = 5
-result = fibonacci_modulo(n, m)
+        if p == 1:
+            return M
+        if p in self.__memo:
+            return self.__memo[p]
+        K = self.__get_matrix_power(M, int(p/2))
+        R = self.__multiply_matrices(K, K)
+        self.__memo[p] = R
+        return R
+
+    def get_number(self, n):
+        """Получение n-го числа Фибоначчи
+        (в качестве n ожидается неотрицательное целое число)."""
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        # Разложение переданной степени на степени, равные степени двойки,
+        # т.е. 62 = 2^5 + 2^4 + 2^3 + 2^2 + 2^0 = 32 + 16 + 8 + 4 + 1.
+        powers = [int(pow(2, b))
+                  for (b, d) in enumerate(reversed(bin(n-1)[2:])) if d == '1']
+        print(powers)
+        # Тоже самое, но менее pythonic: http://pastebin.com/h8cKDkHX
+
+        matrices = [self.__get_matrix_power(MatrixFibonacci.Q, p)
+                    for p in powers]
+        print(matrices)
+        while len(matrices) > 1:
+            M1 = matrices.pop()
+            M2 = matrices.pop()
+            R = self.__multiply_matrices(M1, M2)
+            matrices.append(R)
+        return matrices[0][0][0]
+
+mfib = MatrixFibonacci()
+# number, commits = map(int, '506278  6'.split())
+number, commits = map(int, '10  6'.split())
+# number, commits = map(int, input().split())
+
+num = mfib.get_number(number + 1)
+
+result = num  % (10 ** commits)
 print(result)
--- yandex/sprint_2/fibonacciModule.py
+++ yandex/sprint_2/fibonacciModule.py
@@ -1,6 +1,7 @@
 import time
-import math
+from functools import lru_cache
 
+@lru_cache(maxsize=None)
 def fibonacci(n):
     numbers = [1, 1]
     if n < 2:
@@ -9,29 +10,18 @@ def fibonacci(n):
         counter = 0
         while counter < n - 1:
             summ = (numbers[0] + numbers[1])
-            print(summ)
             numbers[0] = numbers[1]
             numbers[1] = summ
             fib = summ
             counter += 1
         return fib
-
-# def fibonacci(n):
-#     sqrt_5 = math.sqrt(5)
-#     phi = (sqrt_5 + 1) / 2
-#     value = (phi ** n) % 10
-#     return int((value / sqrt_5 + 0.5))
-# def fibonacci(n):
-#     a, b = 0, 1
-#     for _ in range(n):
-#         a, b = b, a + b
-#     return a
+    
 
 # number, commits = map(int, input().split())
-number, commits = map(int, '506277 6'.split())
-number, commits = map(int, '8 6'.split())
+number, commits = map(int, '506277  6'.split())
 
 timeStart = time.time()
+result = fibonacci(number) % (10 ** commits)
 result = fibonacci(number) % (10 ** commits)
 end_time = time.time()
 print(f"Time: {end_time - timeStart}")

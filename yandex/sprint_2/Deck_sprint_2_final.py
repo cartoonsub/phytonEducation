d25931cdba6f980e2cf7c10a164fa5759483a851
Any Task <t.series@yandex.ru>
2024-06-02 00:13:34 +0300

    sprint 2

--- yandex/sprint_2/Deck_sprint_2_final.py
+++ yandex/sprint_2/Deck_sprint_2_final.py
@@ -1,8 +1,5 @@
 import sys
 
-# print((8 + 1) % 10)
-# exit()
-
 class MyQueueSized:
     def __init__(self, n):
         self.queue = [None] * n
@@ -60,23 +57,30 @@ class MyQueueSized:
         
         if self.queue[self.pointer[0]] is None:
             print('error')
+            self.pointer[0] = 0
             return
         
         value = self.queue[self.pointer[0]]
         print(value)
         self.queue[self.pointer[0]] = None
         self.pointer[0] = (self.pointer[0] - 1) % self.size
+        self.current_size -= 1
     
     def pop_back(self):
-        print('index', self.back_index)
-        if self.queue[self.back_index] is None:
+        if self.pointer[1] is None:
             print('error')
             return
         
-        value = self.queue[self.back_index]
+        if self.queue[self.pointer[1]] is None:
+            print('error')
+            self.pointer[1] = 0
+            return
+        
+        value = self.queue[self.pointer[1]]
         print(value)
-        self.queue[self.back_index] = None
-        self.front_index = (self.back_index + 1)
+        self.queue[self.pointer[1]] = None
+        self.pointer[1] = (self.pointer[1] + 1) % self.size
+        self.current_size -= 1
 
 
     def peek(self):
@@ -84,21 +88,17 @@ class MyQueueSized:
 
 
 def main():
-    input_quantity = 7
-    deck_size = 10
-    input = [
-        'pssssush_front -855',
-        'psssush_front 0',
-        'psop_back',
-        'psop_back',
-        # 'push_back 844',
-        # 'pop_back',
-        # 'push_back 823'
-        '',
-        '',
-        ''
-
-    ]
+    # input_quantity = 7
+    # deck_size = 10
+    # input = [
+    #     'push_front -855',
+    #     'push_front 0',
+    #     'pop_back',
+    #     'pop_back',
+    #     'push_back 844',
+    #     'pop_back',
+    #     'push_back 823',
+    # ]
 
     # input_quantity = 4
     # deck_size = 4
@@ -119,22 +119,17 @@ def main():
     #     'pop_front',
     #     'pop_back',
     # ]
-    # 20
-    # 102
-    # input_quantity = int(input())
-    # deck_size = int(input())
 
-    # pop_front() – вывести первый элемент дека и удалить его. Если дек был пуст, то вывести «error».
-    # pop_back() – вывести последний элемент дека и удалить его. Если дек был пуст, то вывести «error».
+    input_quantity = int(input())
+    deck_size = int(input())
+
     Deck = MyQueueSized(deck_size)
 
     for i in range(input_quantity):
-        # line = sys.stdin.readline().rstrip()
-        line = input[i]
+        line = sys.stdin.readline().rstrip()
+        # line = input[i]
         if line == 'pop_front':
             Deck.pop_front()
-        elif line == 'size':
-            print(Deck.size)
         elif line == 'pop_back':
             Deck.pop_back()
         elif 'push_back' in line:
@@ -143,22 +138,5 @@ def main():
             Deck.push_front(int(line.split()[-1]))
 
 
-    Deck.push_front(10)
-    Deck.push_back(12)
-    Deck.push_back(12)
-    Deck.push_front(10)
-    Deck.push_back(12)
-    Deck.push_back(12)
-    Deck.push_back(12)
-    Deck.push_front(10)
-    Deck.push_back(12)
-    Deck.push_front(10)
-    Deck.push_front(10)
-
-
-    Deck.pop_back()
-
-    print(Deck.peek())
-    print(Deck.pointer)
 if __name__ == '__main__':
     main()

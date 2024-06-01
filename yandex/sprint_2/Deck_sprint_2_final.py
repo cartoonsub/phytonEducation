6a4e8236ae78157e52f6d75680e7eeb0722e956b
Any Task <t.series@yandex.ru>
2024-06-02 00:27:59 +0300

    sprint 2

--- yandex/sprint_2/Deck_sprint_2_final.py
+++ yandex/sprint_2/Deck_sprint_2_final.py
@@ -1,5 +1,6 @@
 import sys
 
+
 class MyQueueSized:
     def __init__(self, n):
         self.queue = [None] * n
@@ -49,7 +50,6 @@ class MyQueueSized:
         self.pointer[1] = index
         self.current_size += 1
 
-
     def pop_front(self):
         if self.pointer[0] is None:
             print('error')
@@ -65,6 +65,8 @@ class MyQueueSized:
         self.queue[self.pointer[0]] = None
         self.pointer[0] = (self.pointer[0] - 1) % self.size
         self.current_size -= 1
+        if self.current_size == 0:
+            self.pointer = [None, None]
     
     def pop_back(self):
         if self.pointer[1] is None:
@@ -81,61 +83,30 @@ class MyQueueSized:
         self.queue[self.pointer[1]] = None
         self.pointer[1] = (self.pointer[1] + 1) % self.size
         self.current_size -= 1
-
+        if self.current_size == 0:
+            self.pointer = [None, None]
 
     def peek(self):
         return self.queue
 
 
 def main():
-    # input_quantity = 7
-    # deck_size = 10
-    # input = [
-    #     'push_front -855',
-    #     'push_front 0',
-    #     'pop_back',
-    #     'pop_back',
-    #     'push_back 844',
-    #     'pop_back',
-    #     'push_back 823',
-    # ]
-
-    # input_quantity = 4
-    # deck_size = 4
-    # input = [
-    #     'push_front 861',
-    #     'push_front -819',
-    #     'pop_back',
-    #     'pop_back',
-    # ]
-
-    # input_quantity = 6
-    # deck_size = 6
-    # input = [
-    #     'push_front -201',
-    #     'push_back 959',
-    #     'push_back 102',
-    #     'push_front 20',
-    #     'pop_front',
-    #     'pop_back',
-    # ]
-
     input_quantity = int(input())
     deck_size = int(input())
 
-    Deck = MyQueueSized(deck_size)
+    deck = MyQueueSized(deck_size)
 
     for i in range(input_quantity):
         line = sys.stdin.readline().rstrip()
         # line = input[i]
         if line == 'pop_front':
-            Deck.pop_front()
+            deck.pop_front()
         elif line == 'pop_back':
-            Deck.pop_back()
+            deck.pop_back()
         elif 'push_back' in line:
-            Deck.push_back(int(line.split()[-1]))
+            deck.push_back(int(line.split()[-1]))
         elif 'push_front' in line:
-            Deck.push_front(int(line.split()[-1]))
+            deck.push_front(int(line.split()[-1]))
 
 
 if __name__ == '__main__':

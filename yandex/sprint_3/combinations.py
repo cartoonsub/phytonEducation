c5c94913dce30b4efc2d30e5ed0c694d84af8b3a
Any Task <t.series@yandex.ru>
2024-06-22 18:48:46 +0300

    Refactor Combinators class to improve code readability and performance

--- yandex/sprint_3/combinations.py
+++ yandex/sprint_3/combinations.py
@@ -1,15 +1,40 @@
 
-def generator(n, result = '', counter_open = 0, counter_close = 0):
-    if (n * 2) == counter_open + counter_close:
-        print(result)
-        return
-    
-    if counter_open < n:
-        generator(n, result + '(', counter_open + 1, counter_close)
-
-    if counter_open > counter_close:
-        generator(n, result + ')', counter_open, counter_close + 1)
+class Combinators():
+    def __init__(self) -> None:
+        self.allPosibilities = []
 
 
-count = 3
-generator(count)
+    def getCombinations(self, keys: list) -> str:
+        buttons = {
+            2: ['a', 'b', 'c'],
+            3: ['d', 'e', 'f'],
+            4: ['g', 'h', 'i'],
+            5: ['j', 'k', 'l'],
+            6: ['m', 'n', 'o'],
+            7: ['p', 'q', 'r', 's'],
+            8: ['t', 'u', 'v'],
+            9: ['w', 'x', 'y', 'z'],
+        }
+
+        lettersList = []
+        for key in keys:
+            lettersList.append(buttons[key])
+
+        self.setPosibilities(lettersList)
+        return ' '.join(self.allPosibilities).strip()
+
+    def setPosibilities(self, lettersList, text='', limit=0) -> str:
+        if limit > len(lettersList) - 1:
+            self.allPosibilities.append(text)
+            return ''
+
+        for letter in lettersList[limit]:
+            self.setPosibilities(lettersList, text + letter, limit + 1) + ' '
+        return text
+
+
+input = input()
+input = list(int(i) for i in input)
+
+combinators = Combinators()
+print(combinators.getCombinations(input))

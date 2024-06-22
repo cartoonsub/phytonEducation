d99c1f402d8b90ba7d56e62455bfaa7c65db774a
Any Task <t.series@yandex.ru>
2024-06-23 00:07:46 +0300

    chore: Remove insertion_sort.py

--- yandex/sprint_3/insertion_sort_and_select_sort.py
+++ yandex/sprint_3/insertion_sort_and_select_sort.py
@@ -16,3 +16,18 @@ insertion_sort([11, 2, 9, 7, 1])
 # step 2, sorted 3 elements: [2, 9, 11, 7, 1]
 # step 3, sorted 4 elements: [2, 7, 9, 11, 1]
 # step 4, sorted 5 elements: [1, 2, 7, 9, 11]
+
+
+print('---')
+def select_sort(array):
+    lenght = len(array)
+    for i in range(lenght - 1):
+        print('step:', i, array)
+        min_index = i
+        for k in range(i + 1, lenght):
+            if array[k] < array[min_index]:
+                min_index = k
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
+
+print(select_sort([11, 2, 9, 7, 1]))
